18dfcd1117033d005ee9ea4fa3082e5f65974b63
strata <str.w;f@hotmail.com>
2018-05-25 12:06:31 -0500

    -Registro Hecho

--- app/src/main/java/adr/strata/frida/Register.java
+++ app/src/main/java/adr/strata/frida/Register.java
@@ -1,12 +1,25 @@
 package adr.strata.frida;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstances){
         super.onCreate(savedInstances);
         setContentView(R.layout.register);
+        next = (Button)this.findViewById(R.id.Register_Next1);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent k = new Intent(Register.this,Register2.class);
+                startActivity(k);
+            }
+        });
     }
+
 }
