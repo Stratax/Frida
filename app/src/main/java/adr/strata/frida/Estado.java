75db9dfb55354a7a3133903cb1be78be459381df
Alex <alexjusto10@hotmail.com>
2018-06-05 11:04:07 -0500

    -Funcionalidad en Estado. -Cambio de tipo de sismo en Lobby. -Permisos para SMS y Localización en Manifest.

--- app/src/main/java/adr/strata/frida/Estado.java
+++ app/src/main/java/adr/strata/frida/Estado.java
@@ -1,17 +1,106 @@
 package adr.strata.frida;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageButton;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
-public class Estado extends AppCompatActivity{
+public class Estado extends AppCompatActivity implements View.OnClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback{
+
+    private ImageButton a, b;
+    private LocationManager locManager;
+    private Location loc;
+    private String longit,latit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.estado);
+
+        a = this.findViewById(R.id.btn_aux);
+        b = this.findViewById(R.id.btn_bien);
+
+        RecuperarLocalizacion();
+
+        a.setOnClickListener(this);
+        b.setOnClickListener(this);
+    }
+
+    public void RecuperarLocalizacion(){
+        ActivityCompat.requestPermissions(Estado.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,"Verifica que tengas los permisos requeridos", Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_aux:
+                longit = String.valueOf(loc.getLongitude());
+                latit = String.valueOf(loc.getLatitude());
+                enviarMensajeAux("5533130065","Adrián López",longit,latit);
+            break;
+
+            case R.id.btn_bien:
+                enviarMensajeBien("5533130065","Adrián López");
+            break;
+        }
+    }
+
+    public void enviarMensajeAux(String numero, String nombre, String longitud, String latitud){
+        String datosDelMensaje = "Soy "+nombre+"\n Necesito ayuda, estoy en: "+latitud+", "+longitud;
+        try{
+            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+            if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getApplicationContext(),"No se tiene permiso para enviar SMS",
+                        Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},225);
+            }else{
+                Log.i("Mensaje","Se tienen los permisos");
+            }
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(numero,null,datosDelMensaje,null,null);
+            Toast.makeText(getApplicationContext(),"Mensaje enviado",Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),"Mensaje no enviado",Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void enviarMensajeBien(String numero, String nombre){
+        String datosDelMensaje = "Soy "+nombre+". Me encuentro bien después del sismo.";
+        try{
+            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+            if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(getApplicationContext(),"No se tiene permiso para enviar SMS",
+                        Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},225);
+            }else{
+                Log.i("Mensaje","Se tienen los permisos");
+            }
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(numero,null,datosDelMensaje,null,null);
+            Toast.makeText(getApplicationContext(),"Mensaje enviado",Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),"Mensaje no enviado",Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/adr/strata/frida/Lobby.java
+++ app/src/main/java/adr/strata/frida/Lobby.java
@@ -7,15 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import adr.strata.frida.actores.Usuario;
 
 public class Lobby extends AppCompatActivity implements View.OnClickListener{
-    private ImageButton a, b;
-
+    private ImageButton a, b, c;
+    private ImageView g ,e ,f;
     private Usuario user; //Clase del usuario
+    private int counter = 0;
 
 
     @Override
@@ -33,9 +35,14 @@ public class Lobby extends AppCompatActivity implements View.OnClickListener{
 
         a = this.findViewById(R.id.btn_estado);
         b = this.findViewById(R.id.btn_danio);
+        c = this.findViewById(R.id.logo);
+        g = this.findViewById(R.id.SinSismo);
+        e = this.findViewById(R.id.SismoBajo);
+        f = this.findViewById(R.id.SismoMedio);
 
         a.setOnClickListener(this);
         b.setOnClickListener(this);
+        c.setOnClickListener(this);
     }
 
     @Override
@@ -49,6 +56,32 @@ public class Lobby extends AppCompatActivity implements View.OnClickListener{
                 Intent estado = new Intent(this,Estado.class);
                 startActivity(estado);
                 break;
+            case R.id.logo:
+                counter++;
+                if(counter == 4) {
+                    g.setVisibility(View.VISIBLE);
+                    e.setVisibility(View.INVISIBLE);
+                    f.setVisibility(View.INVISIBLE);
+                    counter = 0;
+                }else if(counter == 1){
+                    g.setVisibility(View.INVISIBLE);
+                    e.setVisibility(View.VISIBLE);
+                    f.setVisibility(View.INVISIBLE);
+                }else if(counter == 2){
+                    g.setVisibility(View.INVISIBLE);
+                    e.setVisibility(View.INVISIBLE);
+                    f.setVisibility(View.VISIBLE);
+                }else if(counter == 3){
+                    g.setVisibility(View.INVISIBLE);
+                    e.setVisibility(View.INVISIBLE);
+                    f.setVisibility(View.INVISIBLE);
+                }
+                break;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
